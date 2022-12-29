a213ba7c65147303af8c18e09851cffc5ee04f6c
diln20 <andreytorres418@gmail.com>
2022-12-29 10:34:17 -0500

    router-custom-hooks

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,24 @@
 
-import React  from 'react';
-import {BrowserRouter,Link, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 const Home = () => <h1>Home</h1>
 
 const Notes = () => <h1>Notes</h1>
 
-const Users= () => <h1>Users</h1>
+const Users = () => <h1>Users</h1>
 
 const inlineStyles = {
   padding: 5
 }
 
 const App = () => {
-   return (
-    <BrowserRouter>
+  return (
+      <BrowserRouter>
       <header>
-        <Link to='/'  style={inlineStyles}>home</Link>
+        <Link to='/' style={inlineStyles}>home</Link>
         <Link to='/notes' style={inlineStyles}>notes</Link>
-        <Link to='/users'  style={inlineStyles}>users</Link>
+        <Link to='/users' style={inlineStyles}>users</Link>
       </header>
       <Route path='/notes'>
         <Notes />
@@ -26,9 +26,9 @@ const App = () => {
       <Route path='/users'>
         <Users />
       </Route>
-       <Route path='/'>
-         <Home />
-       </Route>
+      <Route path='/'>
+        <Home />
+      </Route>
     </BrowserRouter>
   )
 }
